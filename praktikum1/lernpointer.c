5fd0144f5d37c5982beeb3f9a028f575f44d4152
suryaa <146848895+SuryaHub07@users.noreply.github.com>
2024-10-09 01:45:08 +0700

    Add files via upload

---
+++ praktikum1/lernpointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int num=15;
+    int *ptr;//DEKLARASI POINTER BERTIPE INTEGER
+
+    //MENGISI POINTER DENGAN ALAMAT MEMORI VARIABEL NUM
+    ptr=&num;
+
+    printf("Nilai dari num: %d\n", num);
+    printf("Alamat memori dari num: %p\n", &num);//MENGGUNAKAN %p UNTUK MENAMPILKAN ALAMAT MEMORI
+    printf("Nilai yang diakses melalui pointer ptr: %d\n", *ptr);
+    printf("Alamat memori yang disimpan dalam pointer ptr: %p\n", ptr);//MEMILIKI OUTPUT YANG SAMA DENGAN BARIS NO 11 KARENA POINTER *ptr TELAH DIISI DENGAN ALAMAT num
+
+    //MENGUBAH NILAI VARIABEL NUM MEMLALUI POINTER PTR
+    *ptr=25;
+    printf("Nilai baru dari num setelah diubah melalui pointer: %d\n", num);//BERFUNGSI UNTUK MENCETAK NILAI TERBARU DARI VARIABEL num SETELAH DIUBAH MELALUI POINTER *ptr
+
+    return 0;
+}
