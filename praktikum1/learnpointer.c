8387056cedabd5f3e30f4218f70976d1c0dc0db9
suryaa <146848895+LearnWithSuryaa@users.noreply.github.com>
2024-10-09 06:56:14 +0700

    learnpointer.c

--- praktikum1/learnpointer.c
+++ praktikum1/learnpointer.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(){
+/*int main(){
     int num=15;
     int *ptr;//DEKLARASI POINTER BERTIPE INTEGER
 
@@ -15,6 +15,21 @@ int main(){
     //MENGUBAH NILAI VARIABEL NUM MEMLALUI POINTER PTR
     *ptr=25;
     printf("Nilai baru dari num setelah diubah melalui pointer: %d\n", num);//BERFUNGSI UNTUK MENCETAK NILAI TERBARU DARI VARIABEL num SETELAH DIUBAH MELALUI POINTER *ptr
+
+    return 0;
+}*/
+
+void tukar(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(){
+    int x = 10, y =20;
+    tukar(&x, &y);
+    printf("Nilai x yang setelah ditukar: %d\n", x);
+    printf("Nilai y yang setelah ditukar: %d\n",y);
 
     return 0;
 }
