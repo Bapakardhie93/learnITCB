9e79f50eb72c0d238d357bff65eb9c0895442685
suryaa <146848895+LearnWithSuryaa@users.noreply.github.com>
2024-10-20 11:53:08 +0700

    Add files via upload

---
+++ cpmkPromdas/CPMK2/bab2-tipeVariabel/konstanta.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    const int MAXSIZE = 225;
+    const float PI = 3.14, MIN_KELVIN = 80.8, R = 8.314472;
+    const char ROOTDIR = 'C';
+}
---
+++ cpmkPromdas/CPMK2/bab2-tipeVariabel/var.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    int jumlah = 0, nilai_hexa = 0x1a, nilai_oktal = 022;
+    float radius = 0.0;
+    double suhu_awal = 25.0, harga = 10000.00;
+}
